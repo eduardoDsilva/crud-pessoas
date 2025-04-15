71d223206c7cc4b821cbc0c5f804d162b8aab947
Eduardo da Silva <eduardo.2599@hotmail.com>
2025-04-14 23:15:56 -0300

    primeiras tabelas do banco de dados

---
+++ database/migrations/2025_04_15_020522_people.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('people', function (Blueprint $table) {
+            $table->id();
+            $table->string('full_name');
+            $table->string('cpf')->unique();
+            $table->string('rg')->nullable();
+            $table->date('birth_date');
+            $table->string('email')->unique();
+            $table->string('phone')->nullable();
+            $table->enum('gender', ['male', 'female', 'other'])->nullable();
+            $table->string('marital_status')->nullable();
+            $table->string('nationality')->nullable();
+            $table->string('occupation')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('people');
+    }
+};
